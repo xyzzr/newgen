3980a5762109407e6c1fb8ba2d392fabfa768ca3
xyzzr <110189904+xyzzr@users.noreply.github.com>
2025-06-13 15:05:30 +0300

    Update femboify.lua

--- femboify.lua
+++ femboify.lua
@@ -1,10 +1,12 @@
-local c = game:GetService("Players").LocalPlayer.Character or game:GetService("Players").LocalPlayer.CharacterAdded:Wait()
-local heh = c:FindFirstChildOfClass("Pants")
+local x = game:GetService("Players").LocalPlayer
+local y = x.Character:FindFirstChildOfClass("Pants")
+local z = x.Character:FindFirstChildOfClass("Shirt")
 
-if not heh then
-    heh = Instance.new("Pants")
-    heh.Name = "Pants"
-    heh.Parent = c
-end
+if not y then  y = Instance.new("Pants")  y.Name = "Pants" y.Parent = x.Character end
+if not z then z = Instance.new("Shirt") z.Name = "Shirt" z.Parent = x.Character end
 
-heh.PantsTemplate = "rbxassetid://5720984882"
+y.PantsTemplate = "rbxassetid://5720984882"
+z.ShirtTemplate = "rbxassetid://136833414349144"
+
+game.StarterGui:SetCore("SendNotification", { Title = "boy kisser", Text = "you love kissing boys dont you ;3", Duration = 5, Icon = "rbxassetid://81853502728064" })
+print("the owner of 'boy kisser' is not responsible for this script.")
