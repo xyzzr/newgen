501e27877be89f20f9768e4e77535c1e8f3226ec
xyzzr <110189904+xyzzr@users.noreply.github.com>
2025-06-13 17:24:09 +0300

    Update femboi.lua

--- femboi.lua
+++ femboi.lua
@@ -1,12 +1,50 @@
-local x = game:GetService("Players").LocalPlayer
-local y = x.Character:FindFirstChildOfClass("Pants")
-local z = x.Character:FindFirstChildOfClass("Shirt")
+local char = game:GetService("Players").LocalPlayer.Character or game:GetService("Players").LocalPlayer.CharacterAdded:Wait()
 
-if not y then  y = Instance.new("Pants")  y.Name = "Pants" y.Parent = x.Character end
-if not z then z = Instance.new("Shirt") z.Name = "Shirt" z.Parent = x.Character end
+local femboypants = char:FindFirstChildOfClass("Pants")
+local femboyshirt = char:FindFirstChildOfClass("Shirt")
 
-y.PantsTemplate = "rbxassetid://5720984882"
-z.ShirtTemplate = "rbxassetid://136833414349144"
+if not femboypants then femboypants = Instance.new("Pants") femboypants.Name = "Pants" femboypants.Parent = char else print'[+] found pants' end
+if not femboyshirt then femboyshirt = Instance.new("Shirt") femboyshirt.Name = "Shirt" femboyshirt.Parent = char else print'[+] found shirts'  end
+if not char:FindFirstChild("Torso") then print'[!] no torso stopping conversion' return else print'[+] found torso' end -- R6 only!!!!!!
 
-game.StarterGui:SetCore("SendNotification", { Title = "boy kisser", Text = "you love kissing boys dont you ;3", Duration = 5, Icon = "rbxassetid://81853502728064" })
-print("the owner of 'boy kisser' is not responsible for this script.")
+if char:FindFirstChild("Body Colors") then
+    print'[+] found body colors'
+    char["Body Colors"].HeadColor = BrickColor.new("Pastel brown")
+    char["Body Colors"].LeftArmColor = BrickColor.new("Pastel brown")
+    char["Body Colors"].RightArmColor = BrickColor.new("Pastel brown")
+    char["Body Colors"].LeftLegColor = BrickColor.new("Pastel brown")
+    char["Body Colors"].RightLegColor = BrickColor.new("Pastel brown")
+    char["Body Colors"].TorsoColor = BrickColor.new("Pastel brown")
+    print("[+] body colors changed")
+else
+    warn("[!] body colors not found 😂")
+end
+
+
+local found = false
+for _, x in ipairs(char:GetChildren()) do
+    if x:IsA("Accessory") and x.Name == "Pal Hair" then
+		print("[+] found Pal Hair")
+        x.Handle.Mesh.MeshId = "rbxassetid://14871933706"
+        x.Handle.Mesh.TextureId = "rbxassetid://14423578564"
+        x.Handle.Mesh.Scale = Vector3.new(1, 1, 1)
+        x.Handle.Mesh.Offset = Vector3.new(0, 0, 0.25)
+        print("[+] changed Pal Hair")
+        found = true
+        break
+    end
+end
+
+if not found then print("[!] no Pal Hair found") end
+
+local femboytorso = Instance.new("CharacterMesh")
+femboytorso.Parent = char
+femboytorso.MeshId = tonumber("48112070")
+femboytorso.BodyPart = "Torso" 
+print'[+] changed torso'
+
+femboypants.PantsTemplate = "rbxassetid://5720984882" 
+femboyshirt.ShirtTemplate = "rbxassetid://136833414349144"
+print'[+] changed clothes'
+
+game.StarterGui:SetCore("SendNotification", { Title = "faggot", Text = "yea", Duration = 5, Icon = "rbxassetid://81853502728064" })
